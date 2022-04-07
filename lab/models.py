49590dd0961fd2e0da3e267ee837e3d663f1a930
octo-gone <naklimin@miem.hse.ru>
2022-04-07 23:20:14 +0300

    Dice model added

--- lab/models.py
+++ lab/models.py
@@ -23,6 +23,14 @@ class Route:
     def length(self, value):
         pass
 
+    @property
+    def route(self):
+        return self.__route
+
+    @route.setter
+    def route(self, value):
+        pass
+
     def __len__(self):
         return self.__length
 
@@ -68,5 +76,38 @@ class Space:
         return self.__space[item[0]][item[1]]
 
     def __repr__(self):
-        s = '\t\n'.join(map(repr, self.__space))
-        return f"<Space size={self.__size}\n{s}>"
+        return f"<Space size={self.__size}>"
+
+
+class Dice:
+    def __init__(self, change_prob: float = 0.3,
+                 _pool: tuple[int, ...] = None,
+                 _weights: tuple[int, ...] = None):
+
+        if _pool is None:
+            _pool = (0, 1, 2, 3, 4)
+            _weights = (16, 1, 1, 1, 1)
+
+        if _weights is None:
+            _weights = tuple([1 for _ in range(len(_pool))])
+
+        values = [random.randint(1, 6) if random.random() < change_prob else i for i in range(1, 7)]
+        self.__values = []
+        for i, value in enumerate(values):
+            self.__values.append(tuple(random.choices(_pool, weights=_weights, k=value)))
+        self.__values.sort(key=len)
+        self.__values = tuple(self.__values)
+
+    @property
+    def values(self):
+        return self.__values
+
+    @values.setter
+    def values(self, value):
+        pass
+
+    def roll(self):
+        return random.choice(self.__values)
+
+    def __repr__(self):
+        return f"<Dice {self.__values}>"
