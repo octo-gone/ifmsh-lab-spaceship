71430b2081c4aa99c0c101a934c8a8cae916b075
octo-gone <naklimin@miem.hse.ru>
2022-04-07 23:20:28 +0300

    Generator changed

--- lab/generators.py
+++ lab/generators.py
@@ -2,11 +2,12 @@ from . import models
 import random
 
 
-def barabasi_albert(size: int, k: int = 2, route_length: tuple = tuple(range(4, 12))) \
-        -> tuple[tuple[int, int, models.Route], ...]:
+def barabasi_albert(size: int, k: int = 2,
+                    _route_length: tuple = tuple(range(4, 12))) -> tuple[tuple[int, int, models.Route], ...]:
     result = []
     for i in range(size):
-        pool = tuple(set(range(size)) - {i})
-        for j in random.choices(pool, k=k):
-            result.append((i, j, models.Route(random.choice(route_length))))
+        pool = tuple(set(range(i)) - {i})
+        if pool:
+            for j in random.choices(pool, k=min(len(pool), k)):
+                result.append((i, j, models.Route(random.choice(_route_length))))
     return tuple(result)
